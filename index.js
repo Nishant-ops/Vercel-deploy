fb9eb61aecb81f39f6b64180e3f9993988f6e4ae
Nishant <nishantdhamija41@gmail.com>
2024-07-05 15:18:27 +0530

    vercel-deploy

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const app = express();
+app.use(cors());
+
+app.use("/callback/api", (req, res) => {
+  console.log(req.params);
+
+  res.redirect("https://brandcollab.app");
+});
+app.use("/", express.static(path.join(__dirname, "public")));
+app.listen(3000, () => {
+  console.log("h");
+});
