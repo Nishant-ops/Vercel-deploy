39b57a43898234c8bbb9f68ef7453ac75ce43031
Nishant <nishantdhamija41@gmail.com>
2024-07-05 16:25:54 +0530

    nhhh

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 app.use(cors());
 
 app.use("/callback/api", (req, res) => {
+  console.log(req);
   const hash = req.url.split("#")[1];
 
   // Split the hash into key-value pairs
