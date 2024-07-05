f26dc3f76e63582bb69a85089cfa65245046c471
Nishant <nishantdhamija41@gmail.com>
2024-07-05 18:06:26 +0530

    asdasd

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const url = require("url");
 const app = express();
 app.use(cors());
 
 app.use("/callback/api", (req, res) => {
-  console.log(window.location);
-  const hash = req.url.split("#")[1];
+  const queryObject = url.parse(req.url, true).query;
+  const accessToken = queryObject.access_token;
 
   // Split the hash into key-value pairs
-  const params = new URLSearchParams(hash);
+  //   const params = new URLSearchParams(hash);
 
-  // Access individual parameter values
-  const accessToken = params.get("access_token");
-  const dataAccessExpirationTime = params.get("data_access_expiration_time");
-  const expiresIn = params.get("expires_in");
-  console.log(accessToken, dataAccessExpirationTime, expiresIn);
+  //   // Access individual parameter values
+  //   const accessToken = params.get("access_token");
+  //   const dataAccessExpirationTime = params.get("data_access_expiration_time");
+  //   const expiresIn = params.get("expires_in");
+  console.log(accessToken);
 
   return res.json({ a: "func" });
 });
